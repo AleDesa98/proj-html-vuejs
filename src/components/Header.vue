1f561048bf84928a53fa9d69b1bf70b93fc440cd
AleDesa98 <alessandrodesantis1998@gmail.com>
2021-06-21 12:08:24 +0200

    creazione e collegamento components

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Test header</div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Test main</div>
+</template>
+
+<script>
+export default {
+    name: 'Main'
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>

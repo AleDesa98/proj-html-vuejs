59f7c05795cbdbbb0f262213d5093b5081d97b4c
AleDesa98 <alessandrodesantis1998@gmail.com>
2021-06-23 12:16:51 +0200

    finito Section5

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,6 +4,7 @@
         <Section2 />
         <Section3 />
         <Section4 />
+        <Section5 />
     </main>
 </template>
 
@@ -12,6 +13,7 @@ import Section1 from './Section1.vue'
 import Section2 from './Section2.vue'
 import Section3 from './Section3.vue'
 import Section4 from './Section4.vue'
+import Section5 from './Section5.vue'
 
 export default {
     name: 'Main',
@@ -19,7 +21,8 @@ export default {
         Section1,
         Section2,
         Section3,
-        Section4
+        Section4,
+        Section5
     }
 }
 </script>
---
+++ src/components/Section5.vue
@@ -0,0 +1,145 @@
+<template>
+    <div class="section5 container">
+        <div class="top text-center">
+            <div class="category">Locations</div>
+            <div class="title">Visit our Bakeries</div>
+        </div>
+        <div class="bottom">
+            <div class="left">
+                <div class="image"></div>
+                <div class="place">
+                    <div class="name">New York</div>
+                    <div class="description">
+                        <div class="number">
+                            <div class="text">Call us</div>
+                            <div class="contact">1.800.458.556</div>
+                        </div>
+                        <div class="open-day">
+                            <div class="text">Open all week</div>
+                            <div class="contact">9.00 AM - 6:00 PM</div>
+                        </div>
+                        <div class="button">
+                            <span>View Map</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="right">
+                <div class="image"></div>
+                <div class="place">
+                    <div class="name">London</div>
+                    <div class="description">
+                        <div class="number">
+                            <div class="text">Call us</div>
+                            <div class="contact">1.800.458.556</div>
+                        </div>
+                        <div class="open-day">
+                            <div class="text">Open all week</div>
+                            <div class="contact">9.00 AM - 6:00 PM</div>
+                        </div>
+                        <div class="button">
+                            <span>View Map</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Section5'
+}
+</script>
+
+<style scoped lang="scss">
+.section5 {
+    margin-bottom: 100px;
+    .top {
+        padding-top: 60px;
+        & > .category {
+            font-size: 12px;
+            color: grey;
+            text-transform: uppercase;
+        }
+        & > .title {
+            color: #55338B;
+            font-size: 28px;
+            margin: 10px 0 60px 0;
+        }
+    }
+    .bottom {
+        width: 100%;
+        display: flex;
+        & > .left {
+            & > .image {
+                background-image: url(../assets/images/new-york-bk.jpg);
+            }
+            & > .place {
+                background-color: #F0F9FB;
+            }
+        }
+        & > .right {
+            & > .image {
+                background-image: url(../assets/images/london-bk.jpg);
+            }
+            & > .place {
+                background-color: #FAF3E3;
+            }
+        }
+        .left, .right {
+            width: 50%;
+            .image {
+                width: 100%;
+                height: 400px;
+                background-position: center center;
+                background-size: cover;
+                background-repeat: no-repeat;
+            }
+            .place {
+                width: 100%;
+                padding-bottom: 20px;
+                .name {
+                    color: #55338B;
+                    text-align: center;
+                    padding: 20px 0;
+                    font-size: 22px;
+                    font-weight: 500;
+                }
+                .description {
+                    display: flex;
+                    justify-content: space-around;
+                    align-items: center;
+                    text-align: center;
+                    .number, .open-day, .button {
+                        width: calc(100% / 3);
+                    }
+                    .number, .open-day {
+                        color: grey;
+                        .text {
+                            font-size: 10px;
+                            text-transform: uppercase;
+                        }
+                        .contact {
+                            font-size: 14px;
+                        }
+                    }
+                    .button > span {
+                        background-color: white;
+                        color: #55338B;
+                        padding: 10px 20px;
+                        border-radius: 5px;
+                        cursor: pointer;
+                    }
+                    .button > span:hover {
+                        color: white;
+                        background-color: #55338B;
+                    }
+                }
+            }
+        }
+    
+    }
+}
+</style>
